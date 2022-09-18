74cff47472cb8886e5c3b7fcfd275f1d14acf8fb
james <jamesgrieder@cmail.carleton.ca>
2022-09-18 13:18:22 -0400

    Updated main method to create  BuddyInfo, add it to an addressBook and remove it from the same addressBook

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class AddressBook {
     private ArrayList<BuddyInfo> buddies;
 
-    public AddressBook(ArrayList<BuddyInfo> buddies) {
+    public AddressBook() {
         this.buddies = new ArrayList<>();
     }
 
@@ -16,6 +16,9 @@ public class AddressBook {
     }
 
     public static void main(String[] args) {
-        System.out.println("Address Book");
+        BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", "613");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
     }
 }
