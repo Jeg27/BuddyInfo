e1508b62ec5f7e26acc12d79fb0fd0d3f3602e8f
james <jamesgrieder@cmail.carleton.ca>
2022-09-26 10:07:52 -0400

    Updated main method to create  BuddyInfo, add it to an addressBook and remove it from the same addressBook

---
+++ src/BuddyInfo.java
@@ -0,0 +1,29 @@
+public class BuddyInfo {
+
+    private String name;
+    private String address;
+    private String phoneNumber;
+
+    public BuddyInfo(String name, String address, String phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public BuddyInfo() {
+        this.name = "Bob";
+        this.address = "192 Main Street";
+        this.phoneNumber = "613-123-4567";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public static void main(String[] args) {
+
+        BuddyInfo buddy = new BuddyInfo("Homer", "123 Somewhere Lane", "012-345-6789");
+        System.out.println("Hello " + buddy.getName());
+    }
+}
